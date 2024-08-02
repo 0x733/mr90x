27c8dc55467913c2280a33a6ea89ce0fbd6cca6f
0x733 <98838267+0x733@users.noreply.github.com>
2024-08-02 16:17:40 +0300

    up

---
+++ OPENWRT/openwrt.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Yönlendirici bilgileri
+ROUTER_IP="192.168.1.1"
+ROUTER_SSH_PORT="20001"
+ROUTER_PASSWORD="YOUR_ROUTER_PASSWORD"
+
+# OpenWrt dosyaları
+INITRAMFS_FILE="initramfs-kernel.bin"
+SYSUPGRADE_FILE="sysupgrade.bin"
+
+# SSH bağlantısı
+ssh_command() {
+    ssh -p "$ROUTER_SSH_PORT" root@"$ROUTER_IP" "$@"
+}
+
+# Boot indeksi kontrolü
+tp_boot_idx=$(ssh_command "fw_printenv | grep tp_boot_idx")
+
+if [[ $tp_boot_idx == "tp_boot_idx=1" ]]; then
+    echo "Stok yazılımı yüklü. Lütfen web arayüzünden MERCUSYS stok yazılımını yükleyip tekrar deneyin."
+    exit 1
+fi
+
+# 65-iptv dosyasını düzenleme ve telnetd başlatma
+ssh_command "echo 'telnetd -l /bin/login.sh' >> /etc/hotplug.d/iface/65-iptv"
+ssh_command "uci set network.lan.iptv='1' && uci commit network && /etc/init.d/network reload"
+
+# initramfs-kernel.bin dosyasını yükleme
+scp -P "$ROUTER_SSH_PORT" "$INITRAMFS_FILE" root@"$ROUTER_IP":/tmp
+
+# busybox yükleme
+ssh_command "opkg update && opkg install busybox"
+
+# initramfs-kernel.bin boyutunu kontrol etme ve yeni birim oluşturma
+initramfs_size=$(ssh_command "du -h /tmp/$INITRAMFS_FILE | awk '{print $1}'")
+ubi_size=$(echo "$initramfs_size" | sed 's/[^0-9]*//')
+ssh_command "ubirmvol /dev/ubi0 -N kernel && ubimkvol /dev/ubi0 -n 1 -N kernel -s $((ubi_size + 1))MiB"
+
+# OpenWrt initramfs yazma ve yeniden başlatma
+ssh_command "./usr/bin/busybox ubiupdatevol /dev/ubi0_1 /tmp/$INITRAMFS_FILE && reboot"
+
+# OpenWrt env değişkenlerini ayarlama (SSH bağlantısı için biraz bekleme)
+sleep 30
+ssh -p 22 root@"$ROUTER_IP" "fw_setenv baudrate 115200; fw_setenv bootargs 'ubi.mtd=ubi0 console=ttyS0,115200n1 loglevel=8 earlycon=uart8250,mmio32,0x11002000 init=/etc/preinit'; fw_setenv fdtcontroladdr 5ffc0e70; fw_setenv ipaddr 192.168.1.1; fw_setenv loadaddr 0x46000000; fw_setenv mtdids 'spi-nand0=spi-nand0'; fw_setenv mtdparts 'spi-nand0:2M(boot),1M(u-boot-env),50M(ubi0),50M(ubi1),8M(userconfig),4M(tp_data)'; fw_setenv netmask 255.255.255.0; fw_setenv serverip 192.168.1.2; fw_setenv stderr serial@11002000; fw_setenv stdin serial@11002000; fw_setenv stdout serial@11002000; fw_setenv tp_boot_idx 0"
+
+# sysupgrade.bin dosyasını yükleme ve sysupgrade çalıştırma
+scp -P 22 "$SYSUPGRADE_FILE" root@"$ROUTER_IP":/tmp
+ssh -p 22 root@"$ROUTER_IP" "sysupgrade -n /tmp/$SYSUPGRADE_FILE"
+
+echo "OpenWrt kurulumu tamamlandı!"
